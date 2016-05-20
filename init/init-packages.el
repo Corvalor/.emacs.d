8bc4c3ae4b899d46a13d12fa86815648a57ef030
Carsten Knappe <Carsten.Knappe@as-i.com>
2016-05-20 10:06:02 +0200

    Disabled doxymacs

--- init/init-packages.el
+++ init/init-packages.el
@@ -538,12 +538,12 @@
 (require 'gdb-mi)
 (gdb-many-windows)
 
-(require 'doxymacs)
-(add-hook 'c-mode-common-hook 'doxymacs-mode)
-(defun my-doxymacs-font-lock-hook ()
-    (if (or (eq major-mode 'c-mode) (eq major-mode 'c++-mode))
-        (doxymacs-font-lock)))
-(add-hook 'font-lock-mode-hook 'my-doxymacs-font-lock-hook)
+;; (require 'doxymacs)
+;; (add-hook 'c-mode-common-hook 'doxymacs-mode)
+;; (defun my-doxymacs-font-lock-hook ()
+;;     (if (or (eq major-mode 'c-mode) (eq major-mode 'c++-mode))
+;;         (doxymacs-font-lock)))
+;; (add-hook 'font-lock-mode-hook 'my-doxymacs-font-lock-hook)
 
 
 (provide 'init-packages)
