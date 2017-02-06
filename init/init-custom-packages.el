1dea6f9b4cbd470dccb811250b623d9d0f7c1905
Carsten Knappe <Carsten.Knappe@as-i.com>
2017-02-06 09:39:52 +0100

    Rewrote window switching logic for awesome wm

--- init/init-custom-packages.el
+++ init/init-custom-packages.el
@@ -8,16 +8,16 @@
 ;(if (string-prefix-p "i3" (shell-command-to-string "wmctrl -m | sed -n -e 's/^.*Name: //p'"))
     ;(require 'init-i3-packages))
 
-;; (defadvice windmove-do-window-select (around stumpwm-do-window-select-wrapper activate)
-;;   "Let windmove do its own thing, if there is an error, try stumpwm in that direction."
-;;   (condition-case err
-;;       ad-do-it
-;;     (error
-;;      ( let ((dir (cond ((eq (ad-get-arg 0) 'left) "left")
-;;                        ((eq (ad-get-arg 0) 'up) "up")
-;;                        ((eq (ad-get-arg 0) 'down) "down")
-;;                        ((eq (ad-get-arg 0) 'right) "right"))))
-;;          (shell-command (concat "stumpish 'eval (stumpwm::run-commands \"move-focus " dir "\")'" ))))))
+(defadvice windmove-do-window-select (around stumpwm-do-window-select-wrapper activate)
+  "Let windmove do its own thing, if there is an error, try stumpwm in that direction."
+  (condition-case err
+      ad-do-it
+    (error
+     ( let ((dir (cond ((eq (ad-get-arg 0) 'left) "-1")
+                       ((eq (ad-get-arg 0) 'up) "0")
+                       ((eq (ad-get-arg 0) 'down) "0")
+                       ((eq (ad-get-arg 0) 'right) "1"))))
+         (shell-command (concat "echo 'require(\"awful\").screen.focus_relative(" dir ")' | awesome-client" ))))))
 
 (provide 'init-custom-packages)
 ;;; init-custom-packages.el ends here
